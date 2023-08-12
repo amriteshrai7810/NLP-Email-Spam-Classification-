c5179c3ee93584cf4812b7200d873c4fcd64c6a4
Amritesh Rai <amriteshrai7810@gmail.com>
2023-08-12 10:46:14 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.title('Atul salle apni marwa')
+
+col1, col2 = st.columns(2)
+with col1:
+    st.image('https://unsplash.com/photos/WFH4kdnxKbw')
+
+with col2:
+    st.image('https://unsplash.com/photos/XApv6X0ZIEo')
